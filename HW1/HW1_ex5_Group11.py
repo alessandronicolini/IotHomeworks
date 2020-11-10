6bc061a890850c3587d625b2eb80f40ab4b7faf3
Alessandro <alessandronico22@gmail.com>
2020-11-10 12:11:45 +0000

    exploited BytesIO
    frame ByteIo object instantiated befor start sampling, the time seems to be less variable between different runs

--- HW1/HW1_ex5_Group11.py
+++ HW1/HW1_ex5_Group11.py
@@ -1,39 +1,80 @@
 import argparse
+import numpy as np
 import pyaudio
 import time
 from io import BytesIO
+import  subprocess
 
 
 def main():
 
+	# read input
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--num_samples", type=int, help="number of samples to be recorded")
 	parser.add_argument("--output",  type=str, help="directory where results are stored")
 	args = parser.parse_args()
 
+	# define useful variavles
+	chunk = 1024 # length of a chunk
+	channels = 1
+	rate = 48000
+	bytes_per_chunk = 2*chunk # each value is int16, 2 bytes
+	num_chunks = int(rate/chunk) # total number of chunks
+
+	# make a pyaudio instance
 	pa = pyaudio.PyAudio()
 
-	for i in range(args.num_samples):
+	# set freq to min value
+	subprocess.Popen([
+		'sudo',
+		'sh',
+		'-c',
+		"echo powersave > /sxys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+	])
+
+	# initilaize a data container array
+	frame = BytesIO(bytes(bytes_per_chunk*num_chunks))
+	view = frame.getbuffer()
+
+	# cycle for each sample
+	for sample in range(args.num_samples):
+
 		start = time.time()
 
 		# open stream object as input
 		stream = pa.open(
     		format=pyaudio.paInt16,
-   			channels=1,
-    		rate=48000,
+   			channels=channels,
+    		rate=rate,
     		input=True,
-    		frames_per_buffer=1024
+    		frames_per_buffer=chunk
     		)
 
-		frames = []
+		# create audio frame
+		for i in range(num_chunks):
+			view[i*bytes_per_chunk:(i+1)*bytes_per_chunk] = stream.read(chunk)
 
-
-		for i in range(int(48000/1024)):
-			data = stream.read(1024)
-			frames.append(data)
-
+		# stop and close stream
 		stream.stop_stream()
 		stream.close()
+
+		# set max freq
+		subprocess.Popen([
+			'sudo',
+			'sh',
+			'-c',
+			"echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+        ])
+
+		# set low freq
+		subprocess.Popen([
+			'sudo',
+			'sh',
+			'-c',
+			"echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+        ])
+
+		# compute cycle time
 		end = time.time() - start
 		print(end)
 
